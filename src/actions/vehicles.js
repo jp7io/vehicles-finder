4534610032833cf927f3a3f1ec4c6c9f73fc3203
JP Barbosa <jp@jp7.com.br>
2018-04-18 10:27:57 -0300

    Fetch Async Data

--- src/actions/vehicles.js
+++ src/actions/vehicles.js
@@ -1,9 +1,18 @@
-import staticData from '../data/vehicles';
+import axios from 'axios';
 import { FETCH_VEHICLES } from './types';
 
-export const fetchVehicles = () => (
-  {
-    type: FETCH_VEHICLES,
-    payload: staticData.vehicles,
+export const fetchVehicles = (params = {}) => (
+  (dispatch) => {
+    axios.get(`${process.env.REACT_APP_API}/public/cars/search`, {
+      params: {
+        ...params,
+        brand: 'grupo-sinal',
+      },
+    }).then((response) => {
+      dispatch({
+        type: FETCH_VEHICLES,
+        payload: response.data.cars,
+      });
+    });
   }
 );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import reducers from './reducers';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducers);
+const store = createStore(reducers, applyMiddleware(ReduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
