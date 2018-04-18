d83afa4fb6c15f11f5be069fd35082a1e2d01ed5
JP Barbosa <jp@jp7.com.br>
2018-04-18 10:40:48 -0300

    Connect Vehicles to Filters

--- src/components/VehiclesList.js
+++ src/components/VehiclesList.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 import { fetchVehicles } from '../actions/vehicles';
 import Vehicle from './Vehicle';
 
 class VehiclesList extends Component {
   componentWillMount() {
     this.props.fetchVehicles();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.filtersValues !== this.props.filtersValues) {
+      this.props.fetchVehicles(_.pickBy(this.props.filtersValues));
+    }
   }
 
   render() {
@@ -24,6 +31,7 @@ class VehiclesList extends Component {
 
 const mapStateToProps = state => ({
   vehicles: state.vehicles.all,
+  filtersValues: state.filtersValues,
 });
 
 export default connect(mapStateToProps, { fetchVehicles })(VehiclesList);
